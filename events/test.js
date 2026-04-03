f18d122c48f15f29d141c9627f0e7f94be23df71
lx78WyY0J5 <9j2k37st@gmail.com>
2026-04-04 00:10:33 +0200

    logs

--- events/test.js
+++ events/test.js
@@ -7,6 +7,8 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
+        console.log("🪧 Chargement des tests !");
+
         async function test(){
             const guild = client.guilds.cache.get('1489696430758170646');
             const guildCloned = client.guilds.cache.get('1081921426333909072');
@@ -25,6 +27,8 @@ module.exports = {
                 console.log("⚠️ Server not found in cache.");
             }
         }
+
         //test();
+        console.log("🪧 Fin des tests !");
     },
 };
